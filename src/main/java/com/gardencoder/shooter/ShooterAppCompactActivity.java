92519166aa74dc9d4b181a68626a1253749b5e3e
Ramy <ramyatgarden@gmail.com>
2017-09-17 12:29:26 +0300

    [Add] abstract Shoot activity & enable status.

--- src/main/java/com/gardencoder/shooter/ShooterAppCompactActivity.java
+++ src/main/java/com/gardencoder/shooter/ShooterAppCompactActivity.java
@@ -6,30 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import com.gardencoder.shooter.board.ShooterEventListener;
 
 
-public class ShooterAppCompactActivity extends AppCompatActivity {
+public abstract class ShooterAppCompactActivity extends AppCompatActivity {
     public ShooterEventListener shooterEventListener;
+
+    public abstract boolean isEnabled();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        shooterEventListener = new ShooterEventListener(this);
+        if (isEnabled())
+            shooterEventListener = new ShooterEventListener(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        shooterEventListener.onPause();
+        if (shooterEventListener != null)
+            shooterEventListener.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        shooterEventListener.onResume();
+        if (shooterEventListener != null)
+            shooterEventListener.onResume();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
-        shooterEventListener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (shooterEventListener != null)
+            shooterEventListener.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
